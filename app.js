28108324b7609987895b75f14c37bbaff3043d45
B-es <ivan2003064@gmail.com>
2023-02-02 01:09:13 +0300

    Start

---
+++ app.js
@@ -0,0 +1,26 @@
+//Подключение основных библиотек 
+const 
+    express = require('express'),
+    path = require('path'),
+    host = '127.0.0.1',
+    port = 3000,
+    app = express();
+
+//Назначаем папку для статических элементов
+app.use(express.static(path.join(__dirname, "/public")));
+
+//Создаём парсер, который будет читать данные из документа
+const urlencodedParser = express.urlencoded({extended: true});
+
+//Запускаем функцию post с необходимыми параметрами
+app.post("/", urlencodedParser, (req, res) => {   
+    console.log("Отправка заявки...");
+    console.log(req.body.inp);
+    res.redirect("index.html")
+});
+
+
+//Прослушиваем
+app.listen(port, host, () => {
+    console.log("Server was dead before we've started");
+});
