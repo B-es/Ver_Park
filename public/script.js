4185bf49b10936d91eafae0f1fd01a94ff9f57f2
Chilik78 <104494266+Chilik78@users.noreply.github.com>
2023-02-04 16:52:07 +0300

    Добавил форму (Всплывающее окно)

--- public/script.js
+++ public/script.js
@@ -7,3 +7,21 @@ function toForm(){
 }
 
 req.onclick = toForm;
+
+
+//Форма
+var bz=document.querySelector('#hamburger')
+
+bz.onclick=function()
+{
+    document.getElementById('forma').style.display="block"
+    document.getElementById('forma').style.animation= "move-left 1.5s ease-in-out 0s"
+    document.getElementById('forma').style.zIndex="100"
+}
+
+var c=document.querySelector('#closeBtn')
+c.onclick=function()
+{
+    document.getElementById('forma').style.animation= "move-right 1.5s ease-in-out 0s"
+    document.getElementById('forma').style.animationFillMode="forwards"
+}
