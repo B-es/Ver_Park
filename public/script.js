ab9d9be6129963a462c3751de96b56b5e0c135a3
B-es <ivan2003064@gmail.com>
2023-02-28 23:54:52 +0300

    Программы, скроллы, анкета
    Блоки программы начал, начал анкету, вырезал выдвижную форму, добавил скролл от навигации до пунктов

--- public/script.js
+++ public/script.js
@@ -1,31 +1,22 @@
 let req = document.getElementById("req")
 
-function toForm(){
-    let form = document.getElementById("form");
-    let options = {block: "center", inline: "center", behavior: "smooth"};
-    form.scrollIntoView(options);
+function toBlock(block, where="center"){
+    let options = {block: where, inline: "center", behavior: "smooth"};
+    block.scrollIntoView(options);
 }
 
-req.onclick = toForm;
-
-
-//Форма
-var bz=document.querySelector('#hamburger')
-
-bz.onclick=function()
-{
-    document.getElementById('forma').style.display="block"
-    document.getElementById('forma').style.animation= "move-left 1.5s ease-in-out 0s"
-    document.getElementById('forma').style.zIndex="100"
+function toProgram(){
+    let prog = document.getElementById("program");
+    toBlock(prog);
 }
 
-var c=document.querySelector('#closeBtn')
-c.onclick=function()
-{
-    document.getElementById('forma').style.animation= "move-right 1.5s ease-in-out 0s"
-    document.getElementById('forma').style.animationFillMode="forwards"
+function toUs(){
+    let us = document.getElementById("toDiscription");
+    toBlock(us, "start");
 }
 
+document.getElementById("navProg").onclick = toProgram;
+document.getElementById("navUs").onclick = toUs;
 //Загрузочный экран
 
 window.onload = function() 
