c6cd11d5e309f17f4fb01fecdcf1ae9431b3d6d8
B-es <ivan2003064@gmail.com>
2023-03-05 20:20:16 +0300

    Форма и почта
    Переработана форма, теперь заявка отправляется на почту

--- api/api_mail.js
+++ api/api_mail.js
@@ -1,38 +1,38 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 
-let emailAccount = {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-};
-
 //Функция сборки данных из полей в текст для отправки                            
 function compileMessage(data){
-    
+    return {
+        from: '"Сайт Танзания"',
+        to: `${process.env.EMAIL}`,
+        subject: 'Заявка',
+        text: `Имя - ${data.name}\nНомер - ${data.number}\nДата - ${data.date}\n\nВозраст - ${data.age_r}\nКоличество - ${data.count_r}\n`+
+        `Продолжительность - ${data.time_r}`
+    };
 }
 
-async function sendToEmail(email){
-    console.log(email)
+async function sendToEmail(data){
+    let emailAccount = {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    };
+
     let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: email.user,
-        pass: email.pass,
+        user: emailAccount.user,
+        pass: emailAccount.pass,
     },
     });
 
     //Заменить text 
-    let result = await transporter.sendMail({
-        from: '"B_es Production" <Suka@mail.ru>',
-        to: 'sebe@mail.ru',
-        subject: 'Message from Russia',
-        text: 'Sent and dead',
-        html: 'This <b>ras</b> was sent from <strong>bas js</strong> server.'
-    });
+    let result = await transporter.sendMail(compileMessage(data));
 
     console.log(result)
 }
 
-sendToEmail(emailAccount).catch(console.error);
+module.exports.sendToEmail = sendToEmail;
+
 
 //https://ethereal.email/messages
