99a1fb779b5d5b57c6531e40772b79fa13798566
B-es <ivan2003064@gmail.com>
2023-03-07 01:53:21 +0300

    Стабильно

--- api/api_tg_bot.js
+++ api/api_tg_bot.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const TGBot = require('node-telegram-bot-api');
-
-const bot = new TGBot(process.env.TOKEN);
+const bot = new TGBot(process.env.TOKEN, {polling: true});
 let reqs = [];
 
 function compileMessage(data){
